8e2ccccd95a4e303bdd0ab3fbde576ebb0d19ba2
kacsokz <casey.sokach@gmail.com>
2019-08-14 16:25:01 +0000

    Add capability for dynamic tweets

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,62 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+  
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $('#tweet-container').append(createTweetElement(tweet));
+    }
+  }
+
+  const createTweetElement = function(tweet) {
+    let $tweet = $('<article>').addClass('tweet');
+    const markup = $(`
+      <header>
+        <img class="avatar" src="${tweet.user.avatars}"></img>
+          <h3>${tweet.user.name}</h3>
+          <h4>${tweet.user.handle}</h4>
+      </header>
+      <p class="tweet-text">${tweet.content.text}</p>
+      <footer>
+        <p class="tweet-created">${tweet.created_at}</p>
+        <div class="tweet-flags">
+          <i class="far fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="far fa-heart"></i>
+        </div>
+      </footer>
+    `);
+
+    return $tweet.append(markup);
+  }
+
+  renderTweets(data);
+    
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(() => {
   
   $("textarea").keydown(function() {
     // decrement counter
