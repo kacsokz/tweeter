acb9a2aff132a696e444d1172f33c4fb1ad8aeac
kacsokz <casey.sokach@gmail.com>
2019-08-16 04:10:49 +0000

    Final commit, styling

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,45 +3,45 @@
  * jQuery is already loaded
 */
 
-const $form = $('#tweet-form');
-const $errorMsg = $('.errors');
-const $tweetText = $('textarea');
-const $tweetForm = $('.new-tweet');
-
-// ref: https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
-function timeSince(date) {
-  let seconds = Math.floor((new Date() - date) / 1000);
-  let interval = Math.floor(seconds / 31536000);
-  if (interval > 1) {
-    return interval + ' years';
-  }
-  interval = Math.floor(seconds / 2592000);
-  if (interval > 1) {
-    return interval + ' months';
-  }
-  interval = Math.floor(seconds / 86400);
-  if (interval > 1) {
-    return interval + ' days';
-  }
-  interval = Math.floor(seconds / 3600);
-  if (interval > 1) {
-    return interval + ' hours';
-  }
-  interval = Math.floor(seconds / 60);
-  if (interval > 1) {
-    return interval + ' minutes';
-  }
-  return Math.floor(seconds) + ' seconds';
-}
-
-// sanitize user input
-const escape =  function(str) {
-  let div = document.createElement('div');
-  div.appendChild(document.createTextNode(str));
-  return div.innerHTML;
-};
-
 $(() => {
+
+  const $form = $('#tweet-form');
+  const $errorMsg = $('.errors');
+  const $tweetText = $('textarea');
+  const $tweetForm = $('.new-tweet');
+
+  // ref: https://stackoverflow.com/questions/3177836/how-to-format-time-since-xxx-e-g-4-minutes-ago-similar-to-stack-exchange-site
+  function timeSince(date) {
+    let seconds = Math.floor((new Date() - date) / 1000);
+    let interval = Math.floor(seconds / 31536000);
+    if (interval > 1) {
+      return interval + ' years';
+    }
+    interval = Math.floor(seconds / 2592000);
+    if (interval > 1) {
+      return interval + ' months';
+    }
+    interval = Math.floor(seconds / 86400);
+    if (interval > 1) {
+      return interval + ' days';
+    }
+    interval = Math.floor(seconds / 3600);
+    if (interval > 1) {
+      return interval + ' hours';
+    }
+    interval = Math.floor(seconds / 60);
+    if (interval > 1) {
+      return interval + ' minutes';
+    }
+    return Math.floor(seconds) + ' seconds';
+  }
+
+  // sanitize user input
+  const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
   // fetch and render tweets
   const loadTweets = function() {
